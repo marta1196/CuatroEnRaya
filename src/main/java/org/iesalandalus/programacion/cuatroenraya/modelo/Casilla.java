2fd46528deb3140e5e6a7e560160303282f170a4
marta <marta@DESKTOP-TMB7CRH.home>
2021-01-27 22:05:04 +0100

    Creación del método estaOcupada().

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -1,6 +1,23 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Casilla {
 
 	private Ficha ficha;
+
+	public boolean estaOcupada() {
+
+		boolean estaOcupadaCasilla = true;
+
+		if (ficha == null) {
+
+			estaOcupadaCasilla = false;
+
+		} else {
+			estaOcupadaCasilla = true;
+		}
+
+		return estaOcupadaCasilla;
+	}
 }
