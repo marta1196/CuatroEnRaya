0e3d2ebf0e7e6d595160004463e1677ee72dcc53
marta <marta@DESKTOP-TMB7CRH.home>
2021-01-28 00:28:09 +0100

    Creación de los métodos get y set para los atributos colorFichas y nombre.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
@@ -4,4 +4,34 @@ public class Jugador {
 
 	private Ficha colorFichas;
 	private String nombre;
+
+	public Ficha getColorFichas() {
+		return colorFichas;
+	}
+
+	private void setColorFichas(Ficha colorFichas) {
+
+		if (colorFichas == null) {
+
+			throw new NullPointerException("ERROR: El color de las fichas no puede ser nulo.");
+		}
+		this.colorFichas = colorFichas;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+
+		if (nombre == null) {
+
+			throw new NullPointerException("ERROR: El nombre no puede ser nulo.");
+
+		} else if (nombre.trim().equals("")) {
+
+			throw new IllegalArgumentException("ERROR: El nombre no puede estar vacío.");
+		}
+		this.nombre = nombre;
+	}
 }
