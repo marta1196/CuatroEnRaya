d2e1682f7b44f22b596e0acb0d5669657c066d58
marta <marta@DESKTOP-TMB7CRH.home>
2021-01-28 00:57:20 +0100

    Creación del constructor por defecto de la clase Tablero.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
@@ -6,4 +6,16 @@ public class Tablero {
 	public static final int COLUMNAS = 7;
 	public static final int FICHAS_IGUALES_CONSECUTIVAS_NECESARIAS = 4;
 	private Casilla[][] casillas;
+
+	public Tablero() {
+
+		casillas = new Casilla[FILAS][COLUMNAS];
+
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+
+				casillas[i][j] = new Casilla();
+			}
+		}
+	}
 }
