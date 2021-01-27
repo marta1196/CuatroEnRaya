e4718b6cfc789ae472ac4320bcdd1ec93c268852
marta <marta@DESKTOP-TMB7CRH.home>
2021-01-28 00:56:21 +0100

    Creación de la clase Tablero con los atributos: FILAS, COLUMNAS, FICHA_IGUALES_CONSECUTIVAS_NECESARIAS y casillas.

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+public class Tablero {
+
+	public static final int FILAS = 6;
+	public static final int COLUMNAS = 7;
+	public static final int FICHAS_IGUALES_CONSECUTIVAS_NECESARIAS = 4;
+	private Casilla[][] casillas;
+}
