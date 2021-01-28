7b9875d55f0476813d1eaeb1be9c65604f8f3112
marta <marta@DESKTOP-TMB7CRH.home>
2021-01-28 02:30:39 +0100

    Creación de la clase CuatroEnRaya con los atributos: NUMERO_JUGADORES, jugadores y tablero.

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/CuatroEnRaya.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.cuatroenraya;
+
+import org.iesalandalus.programacion.cuatroenraya.modelo.Jugador;
+import org.iesalandalus.programacion.cuatroenraya.modelo.Tablero;
+
+public class CuatroEnRaya {
+
+	private static final int NUMERO_JUGADORES = 2;
+	private Jugador[] jugadores;
+	private Tablero tablero;
+}
