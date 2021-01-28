50e6a529af5e84c6567ebf1d00c7b118040651b0
marta <marta@DESKTOP-TMB7CRH.home>
2021-01-28 02:19:47 +0100

    Creación del método leerNombre.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/vista/Consola.java
@@ -1,8 +1,24 @@
 package org.iesalandalus.programacion.cuatroenraya.vista;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
 
 	private Consola() {
 
+	}
+
+	public static String leerNombre() {
+
+		String nombre;
+
+		do {
+
+			System.out.print("Introduce el nombre del jugador: ");
+			nombre = Entrada.cadena();
+
+		} while (nombre == null || nombre == "");
+
+		return nombre;
 	}
 }
