12b685fd22a387af342d6e04da253963037cb724
marta <marta@DESKTOP-TMB7CRH.home>
2021-01-28 02:32:25 +0100

    Creación de un constructor con los parámetros primerJugador y segundoJugador.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/CuatroEnRaya.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/CuatroEnRaya.java
@@ -8,4 +8,18 @@ public class CuatroEnRaya {
 	private static final int NUMERO_JUGADORES = 2;
 	private Jugador[] jugadores;
 	private Tablero tablero;
+
+	public CuatroEnRaya(Jugador primerJugador, Jugador segundoJugador) {
+
+		if (primerJugador == null || segundoJugador == null) {
+
+			throw new NullPointerException("ERROR: Ningún jugador puede ser nulo");
+		}
+
+		tablero = new Tablero();
+		jugadores = new Jugador[NUMERO_JUGADORES];
+		jugadores[0] = primerJugador;
+		jugadores[1] = segundoJugador;
+
+	}
 }
