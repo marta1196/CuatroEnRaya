f93eee245906e90c7135c54643855c73cb2d2624
marta <marta@DESKTOP-TMB7CRH.home>
2021-01-28 02:21:15 +0100

    Creación del método elegirColorFichas.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/vista/Consola.java
@@ -1,5 +1,6 @@
 package org.iesalandalus.programacion.cuatroenraya.vista;
 
+import org.iesalandalus.programacion.cuatroenraya.modelo.Ficha;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Consola {
@@ -20,5 +21,38 @@ public class Consola {
 		} while (nombre == null || nombre == "");
 
 		return nombre;
+	}
+	
+	public static Ficha elegirColorFichas() {
+
+		Ficha colorFicha = null;
+		int opcionColor = -1;
+
+		do {
+
+			System.out.print("Elige el color de tus fichas (0-AZUL, 1-VERDE): ");
+			opcionColor = Entrada.entero();
+
+			switch (opcionColor) {
+
+			case 0:
+
+				colorFicha = colorFicha.AZUL;
+				break;
+
+			case 1:
+
+				colorFicha = colorFicha.VERDE;
+				break;
+
+			default:
+
+				System.out.println("Lo siento pero esa opción no esta permitida");
+				break;
+			}
+
+		} while (opcionColor != 0 && opcionColor != 1);
+
+		return colorFicha;
 	}
 }
